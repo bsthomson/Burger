7f13166ffe43efde7d326d342b79b1bda59ae746
bsthomson <thomson.bs@gmail.com>
2018-08-18 14:51:11 -0500

    changes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,1 +1,43 @@
 const express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+        "burger_name", "consumed"
+    ], [
+        req.body.burger_name, req.body.consumed 
+    ], function (result) {
+        res.json({ id: result.InsertId})
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        consumed: req.body.consumed
+    }, condition, function (result) {
+        if (result.changedRows === 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
